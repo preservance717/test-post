7a4065f12bbccda3d6f2a5332d504d4ee83896e9
preservance717 <GL717@outlook.com>
2016-05-18 20:56:12 +0800

    update

--- src/collect_all_even.js
+++ src/collect_all_even.js
@@ -1,6 +1,17 @@
-
 function collect_same_elements(collection_a, object_b) {
- //在此处写代码
+    var result = [];
+
+    collection_a.forEach(
+        function equal(element) {
+            for(var i = 0; i < object_b.value.length; i++){
+                if(element.key === object_b.value[i]){
+                    result.push(element.key);
+                }
+            }
+        }
+    );
+
+    return result;
 }
 
 module.exports = collect_same_elements;
